cf7d52c019971b4594e1c31256650e85b4ada83e
Nick <nick@npsoftware.co.uk>
2020-04-06 10:56:23 +0100

    Tweaked thrust and rotation mechanisms.

--- Project Boost/Assets/Rocket.cs
+++ Project Boost/Assets/Rocket.cs
@@ -5,6 +5,7 @@ public class Rocket : MonoBehaviour
 {
     private Rigidbody _rigidBody;
     private AudioSource _audioSource;
+    private bool _applyThrust;
 
     void Start()
     {
@@ -28,8 +29,14 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        bool thrust = Input.GetKey(KeyCode.Space);
-        if (thrust)
+        ApplyThrust();        
+        ApplyRotation();
+    }
+
+    private void ApplyThrust()
+    {
+        _applyThrust = Input.GetKey(KeyCode.Space);
+        if (_applyThrust)
         {
             _rigidBody.AddRelativeForce(Vector3.up);
             if (!_audioSource.isPlaying)
@@ -41,19 +48,31 @@ public class Rocket : MonoBehaviour
         {
             _audioSource.Stop();
         }
+    }
 
-        var rotateLeft = Input.GetKey(KeyCode.A);
-        var rotateRight = Input.GetKey(KeyCode.D);
-        if(!(rotateLeft && rotateRight))
+    private void ApplyRotation()
+    {
+        try
         {
-            if (rotateLeft)
+            _rigidBody.freezeRotation = true;
+
+            var rotateLeft = Input.GetKey(KeyCode.A);
+            var rotateRight = Input.GetKey(KeyCode.D);
+            if (_applyThrust && !(rotateLeft && rotateRight))
             {
-                transform.Rotate(Vector3.forward);
+                if (rotateLeft)
+                {
+                    transform.Rotate(Vector3.forward);
+                }
+                else if (rotateRight)
+                {
+                    transform.Rotate(-Vector3.forward);
+                }
             }
-            else if (rotateRight)
-            {
-                transform.Rotate(-Vector3.forward);
-            }
+        }
+        finally
+        {
+            _rigidBody.freezeRotation = false;
         }
     }
 }
