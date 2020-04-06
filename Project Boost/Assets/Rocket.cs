dc51f9c60c4e31f87cdb668a9cd13f7adebd6bca
Nick <nick@npsoftware.co.uk>
2020-04-06 09:59:01 +0100

    Added thrust and rotation to rocket.

---
+++ Project Boost/Assets/Rocket.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody _rigidBody;
+
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        if(_rigidBody == null)
+        {
+            throw new InvalidOperationException($"Parent of '{name}' does not have a rigid body component.");
+        }
+    }
+
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        bool thrust = Input.GetKey(KeyCode.Space);
+        if (thrust)
+        {
+            _rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        var rotateLeft = Input.GetKey(KeyCode.A);
+        var rotateRight = Input.GetKey(KeyCode.D);
+        if(!(rotateLeft && rotateRight))
+        {
+            if (rotateLeft)
+            {
+                transform.Rotate(Vector3.forward);
+            }
+            else if (rotateRight)
+            {
+                transform.Rotate(-Vector3.forward);
+            }
+        }
+    }
+}
