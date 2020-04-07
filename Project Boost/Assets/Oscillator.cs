87ebe6b314efc11682c3c16390351121c4a12572
Nick <nick@npsoftware.co.uk>
2020-04-07 12:11:01 +0100

    Created ossilator.

---
+++ Project Boost/Assets/Oscillator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [Range(0, 1)]
+    [SerializeField] float MovementFactor;
+    [SerializeField] Vector3 MovementVector;
+    [SerializeField] bool Enabled = true;
+    [SerializeField] float Period = 2.0f;
+
+    private const float _tau = Mathf.PI * 2;
+    private Vector3 _startPosition;
+
+    void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if(Enabled)
+        {
+            float cycles = Time.time / Period;
+            float rawSinWave = Mathf.Sin(cycles * _tau);
+            MovementFactor = rawSinWave / 2f + 0.5f;
+        }
+        Vector3 offset = MovementVector * MovementFactor;
+        transform.position = _startPosition + offset;
+    }
+}
